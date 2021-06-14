b6b748c26401e8b0896ac0969269773edeafedb4
johishinuma <johishinuma@gmail.com>
2021-06-14 11:06:22 +0900

    adding integrates

--- unittests/integtests/test_service.py
+++ unittests/integtests/test_service.py
@@ -1,7 +1,6 @@
 import requests
 import os
 
-
 def test_200_response():
-    with requests.get(os.environ['SERVICE_URL']) as response:
-        assert response.status_code == 200
+  with requests.get(os.environ['SERVICE_URL']) as response:
+    assert response.status_code == 200
