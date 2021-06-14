f0401ab67700de8842af8d47b9e0d1d5b0b84446
johishinuma <johishinuma@gmail.com>
2021-06-14 10:58:14 +0900

    adding integrates

--- cdk_piplines_webinar/pipeline_stack.py
+++ cdk_piplines_webinar/pipeline_stack.py
@@ -33,15 +33,26 @@ class PipelineStack(core.Stack):
 
                 )
 
-            
-        pre_prod_stage = pipeline.add_application_stage(WebServiceStage(self, 'Pre-prod', env= {
+
+        pre_prod_app = WebServiceStage(self, 'Pre-prod', env= {
             'account': '652846672077',
             'region': 'ap-northeast-1',
-        }))
+        })
+            
+        pre_prod_stage = pipeline.add_application_stage(pre_prod_app)
 
-        pre_prod_stage.add_manual_approval_action(
-            action_name='PromoteToProd'
-        )
+        pre_prod_stage.add_actions(pipelines.ShellScriptAction(
+            action_name='Integ',
+            run_order=pre_prod_stage.next_sequential_run_order(),
+            additional_artifacts=[source_artifact],
+            commands=[
+                'pip install -r requirements.txt',
+                'pytest integtests',
+            ],
+            use_outputs={
+                'SERVICE_URL': pipeline.stack_output(pre_prod_app.url_output)
+            }
+        ))
 
         pipeline.add_application_stage(WebServiceStage(self, 'Prod', env= {
             'account': '652846672077',
